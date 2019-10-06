3a651dfcabfb6a1a00ba36c6aa64f6e233789709
赵贺 <1013909206@qq.com>
2019-10-06 16:10:49 +0800

    add event helper

---
+++ zwtimecpp/core/utils/event_helper.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "zwtimecpp/core/utils/memory_utils.hpp"
+#define START_PROCESS_EVENT(baseEvent)            \
+  {                                               \
+    shared_ptr<BaseEvent> pbaseEvent = baseEvent; \
+    if (false) {
+#define CASE_EVENT(eventType)             \
+  }                                       \
+  else if (instanceof <eventType>(ptr)) { \
+    shared_ptr<eventType> event = dpc<eventType>(pbaseEvent);
+
+#define END_PROCESS_EVENT() \
+  }                         \
+  }
+
+/**/

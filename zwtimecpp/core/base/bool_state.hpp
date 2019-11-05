59c616bc6e6cb86f6c0a9fc54604fc05e9e7941e
赵贺 <1013909206@qq.com>
2019-11-05 13:28:56 +0800

    add bool state

---
+++ zwtimecpp/core/base/bool_state.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "zwtimecpp/core/utils/better-enums/enum.h"
+namespace zwsd {
+namespace core {
+using namespace std;
+BETTER_ENUM(BoolState_t, int, set, unSet);
+}
+}  // namespace zwsd

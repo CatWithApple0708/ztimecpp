db1d2cfb0a1b5adb0defe9f3128f9e8bff4dfd6e
赵贺 <1013909206@qq.com>
2020-01-04 21:09:56 +0800

    update cli_strong_version

--- zwtimecpp/core/utils/cli_strong_version.hpp
+++ zwtimecpp/core/utils/cli_strong_version.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cli/cliasyncsession.h>
 #include <cli/clifilesession.h>
+#include <cli/remotecli.h>
 #include "cli/cli.h"
 #include "cli/clilocalsession.h"  // include boost asio
 
@@ -18,6 +19,21 @@
   boost::asio::io_service::work work(ioc);                                 \
   ioc.run();
 
+#define CLI_START_AS_SERVER(rootMenu, __port)                              \
+  boost::asio::io_service ioc;                                             \
+  cli::Cli clivar(std::move(rootMenu));                                    \
+  clivar.ExitAction(                                                       \
+      [](auto &out) { out << "Goodbye and thanks for all the fish.\n"; }); \
+  cli::CliLocalTerminalSession localSession(clivar, ioc, std::cout, 200);  \
+  localSession.ExitAction([&ioc](auto &out) {                              \
+    out << "Closing App...\n";                                             \
+    ioc.stop();                                                            \
+  });                                                                      \
+  cli::CliTelnetServer cli_server(ioc,__port, clivar);                    \
+  cli_server.ExitAction(                                                   \
+      [](auto &out) { out << "Terminating this session...\n"; });          \
+  ioc.run();
+
 #else
 
 #define CLI_START(rootMenu)                                                \
@@ -31,6 +47,21 @@
     ioc.stop();                                                            \
   });                                                                      \
   auto work = boost::asio::make_work_guard(ioc);                           \
+  ioc.run();
+
+#define CLI_START_AS_SERVER(rootMenu, __port)                              \
+  boost::asio::io_context ioc;                                             \
+  cli::Cli clivar(std::move(rootMenu));                                    \
+  clivar.ExitAction(                                                       \
+      [](auto &out) { out << "Goodbye and thanks for all the fish.\n"; }); \
+  cli::CliLocalTerminalSession localSession(clivar, ioc, std::cout, 200);  \
+  localSession.ExitAction([&ioc](auto &out) {                              \
+    out << "Closing App...\n";                                             \
+    ioc.stop();                                                            \
+  });                                                                      \
+  cli::CliTelnetServer cli_server(ioc,__port, clivar);                    \
+  cli_server.ExitAction(                                                   \
+      [](auto &out) { out << "Terminating this session...\n"; });          \
   ioc.run();
 
 #endif
